2c48bb3d6973b857e7512d49b11dcd9b60a44509
kazanik91 <plhooper91@gmail.com>
2023-09-07 00:30:22 +0200

    init commit for rabbitmq spring boot demo repo project

---
+++ src/main/java/pl/kazanik/rabbitmq/demo/Application.java
@@ -0,0 +1,13 @@
+package pl.kazanik.rabbitmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
