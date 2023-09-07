da40fdb6387e7ff5976d9fad8cb87e292d79d381
kazanik91 <plhooper91@gmail.com>
2023-09-07 02:06:50 +0200

    Created simple rest api for getting message from request query parameter and then publishing it to the rabbit mq queue.

---
+++ src/main/java/pl/kazanik/rabbitmq/demo/controller/RabbitMQController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.kazanik.rabbitmq.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.kazanik.rabbitmq.demo.producer.RabbitMQProducer;
+
+/**
+ *
+ * @author Krysia
+ */
+@RestController
+@RequestMapping(value = "/api/rabbit-demo")
+public class RabbitMQController {
+    
+    private RabbitMQProducer rabbitProducer;
+
+    public RabbitMQController(RabbitMQProducer rabbitProducer) {
+        this.rabbitProducer = rabbitProducer;
+    }
+    
+    @GetMapping(path = "/publish")
+    public ResponseEntity<String> publishMessage(
+            @RequestParam("message") String message) {
+        rabbitProducer.sendMessage(message);
+        return ResponseEntity.ok("Message published to the rabbit mq...");
+    }
+}
